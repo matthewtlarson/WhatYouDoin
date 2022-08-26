f97956b4383f1223e3aa8398ee8ee6f4a979c7e7
Maximus Cristian <mcristia2938@gmail.com>
2022-08-26 10:05:59 -0700

    added a final footer, added buttons to the login and logout test and signup

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,40 +1,45 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
-import logo from "../../assets/img/logo.png"
+import logo from "../../assets/img/logo.png";
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
         <ul className="flex-row">
-          <li className="ml-1 mt-2 p-1">
-            <Link to="/orderHistory">
-              Test Tab
-            </Link>
-          </li>
-          <li className="mx-3 mt-2 p-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
+          <button type="button" className="btn btn-warning mt-4">
+            <li className="mx-3 mt-2 p-1">
+              {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+              <a href="/" onClick={() => Auth.logout()}>
+                Logout
+              </a>
+            </li>
+          </button>
+          <button type="button" className="btn btn-warning mt-4">
+            <li className=" mt-2 p-1">
+              <Link to="/orderHistory">Test Tab</Link>
+            </li>
+          </button>
         </ul>
       );
     } else {
       return (
         <ul className="flex-row">
-          <li className="ml-3 mt-2 p-1">
-            <Link to="/login" style={{ textDecoration: 'none'}}>
-              Login
-            </Link>
-          <li className="mx-3 mt-2 p-1">
-            <Link to="/signup" style={{ textDecoration: 'none'}}>
-              Signup
-            </Link>
-          </li>
-          </li>
+          <button type="button" className="btn btn-warning mt-4">
+            <li className="ml-3 mt-2 p-1">
+              <Link to="/login" style={{ textDecoration: "none" }}>
+                Login
+              </Link>
+            </li>
+          </button>
+          <button type="button" className="btn btn-warning mt-4">
+            <li className="ml-3 mt-2 p-1">
+              <Link to="/signup" style={{ textDecoration: "none" }}>
+                Signup
+              </Link>
+            </li>
+          </button>
         </ul>
       );
     }
@@ -43,14 +48,16 @@ function Nav() {
   return (
     <header className="flex-row px-5 justify-content-end">
       <h1>
-        <Link to="/" style={{ textDecoration: 'none'}}>
-          <img src={logo} alt="logo" className='mt-1 mx-1 position-absolute top-0 start-0'/>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <img
+            src={logo}
+            alt="logo"
+            className="mt-1 mx-1 position-absolute top-0 start-0"
+          />
         </Link>
       </h1>
 
-      <nav>
-        {showNavigation()}
-      </nav>
+      <nav>{showNavigation()}</nav>
     </header>
   );
 }
