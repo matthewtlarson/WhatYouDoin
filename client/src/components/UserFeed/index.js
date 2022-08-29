57ab123f7eae5c5a1760e314e34b4efa6b311882
BrandtD <brandtdavidson@gmail.com>
2022-08-29 14:00:41 -0600

    reloading works

--- client/src/components/UserFeed/index.js
+++ client/src/components/UserFeed/index.js
@@ -70,17 +70,18 @@ const UserFeed = () => {
       setDate(value);
     } else if (name === "address") {
       setAddress(value);
-    } 
-    
+    }
   };
 
   const [open, setOpen] = useState(false);
 
+  function refreshComponent() {
+    window.location.reload(false);
+  }
+
   const profile = Auth.getProfile();
   const { loading, data } = useQuery(QUERY_USER_DATA, {
-    variables: { email: profile.data.email}
-   
-    ,
+    variables: { email: profile.data.email },
   });
 
   const user = data?.me || data?.user || {};
@@ -126,58 +127,58 @@ const UserFeed = () => {
         </Button>
         <Collapse in={open}>
           <div id="example-collapse-text">
-            <form style={{ width: "100%", backgroundColor: "#979dac" }} onSubmit={handleFormSubmit}>
-              <div
-                className="form-group border border-primary p-3 mb-2 text-dark text-center collpase"
-                
-              >
+            <form
+              style={{ width: "100%", backgroundColor: "#979dac" }}
+              onSubmit={handleFormSubmit}
+            >
+              <div className="form-group border border-primary p-3 mb-2 text-dark text-center collpase">
                 <div>
-                <textarea
-                  // name="text"
-                  name="title"
-                  // class="form-control"
-                  className="form-input"
-                  style={{ height: "100px" }}
-                  placeholder="Title"
-                  value={title}
-                  onChange={handleChange}
-                ></textarea>
+                  <textarea
+                    // name="text"
+                    name="title"
+                    // class="form-control"
+                    className="form-input"
+                    style={{ height: "100px" }}
+                    placeholder="Title"
+                    value={title}
+                    onChange={handleChange}
+                  ></textarea>
                 </div>
                 <div>
-                <textarea
-                  // name="text"
-                  name="description"
-                  // class="form-control"
-                  className="form-input"
-                  style={{ height: "100px" }}
-                  placeholder="Description of Event"
-                  value={description}
-                  onChange={handleChange}
-                ></textarea>
+                  <textarea
+                    // name="text"
+                    name="description"
+                    // class="form-control"
+                    className="form-input"
+                    style={{ height: "100px" }}
+                    placeholder="Description of Event"
+                    value={description}
+                    onChange={handleChange}
+                  ></textarea>
                 </div>
                 <div>
-                <textarea
-                  // type="text"
-                  name='date'
-                  // class="form-control"
-                  className='form-input'
-                  style={{ height: "100px" }}
-                  placeholder="Date/Time of your event?"
-                  value={date}
-                  onChange={handleChange}
-                ></textarea>
+                  <textarea
+                    // type="text"
+                    name="date"
+                    // class="form-control"
+                    className="form-input"
+                    style={{ height: "100px" }}
+                    placeholder="Date/Time of your event?"
+                    value={date}
+                    onChange={handleChange}
+                  ></textarea>
                 </div>
                 <div>
-                <textarea
-                  // type="text"
-                  name='address'
-                  // class="form-control"
-                  className='form-input'
-                  style={{ height: "100px" }}
-                  placeholder="Event Location"
-                  value={address}
-                  onChange={handleChange}
-                ></textarea>
+                  <textarea
+                    // type="text"
+                    name="address"
+                    // class="form-control"
+                    className="form-input"
+                    style={{ height: "100px" }}
+                    placeholder="Event Location"
+                    value={address}
+                    onChange={handleChange}
+                  ></textarea>
                 </div>
                 {/* <div class="form-check form-check-inline">
                   <input
@@ -206,6 +207,7 @@ const UserFeed = () => {
               </div>
               <button
                 type="submit"
+                onClick={refreshComponent}
                 className="btn btn-primary p-3 mb-2 bg-danger text-white"
                 style={{ width: "50%", marginLeft: "25%" }}
               >
@@ -216,7 +218,7 @@ const UserFeed = () => {
         </Collapse>
 
         <div style={{ background: "grey" }}>
-          <UserEventsFeed events={user.events} username={user.username}/>
+          <UserEventsFeed events={user.events} username={user.username} />
         </div>
       </div>
     </>
